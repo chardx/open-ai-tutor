e3f662cfa3267e8ff3f9c43a25ab368548656f7a
Richard Roxas <chardsourcex@gmail.com>
2023-03-03 14:50:28 +0800

    added more logs for using cron-jobs

--- routes/run-command.js
+++ routes/run-command.js
@@ -8,7 +8,9 @@ router.get('/', async (req, res) => {
     // Code to run your command goes here
     const newTutorial = await getTutorial();
     console.log(newTutorial)
-    res.send('Command executed successfully!');
+    res.write(newTutorial);
+    res.write('Command executed successfully!')
+    res.send();
 });
 
 export default router
