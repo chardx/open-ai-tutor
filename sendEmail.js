6830020047a65b931ef38940e392f04daeda0594
Richard Roxas <chardsourcex@gmail.com>
2023-03-05 05:41:26 +0800

    revised catching error

--- sendEmail.js
+++ sendEmail.js
@@ -1,16 +1,14 @@
 import { transporter, mailOptions } from "./nodemailer.js";
 
 export const sendEmail = async (generatedTutorial, keyword, category) => {
-    try {
-        await transporter.sendMail({
-            ...mailOptions,
-            subject: `${category} about ${keyword}`,
-            text: generatedTutorial,
-            html: `<h3> ${category} </h3> <p>${generatedTutorial}</p>`,
-        })
-    } catch (error) {
-        console.log(error);
-    }
+
+    await transporter.sendMail({
+        ...mailOptions,
+        subject: `${category} about ${keyword}`,
+        text: generatedTutorial,
+        html: `<h3> ${category} </h3> <p>${generatedTutorial}</p>`,
+    }).catch((error) => console.log(error));
+
 
 }
 
